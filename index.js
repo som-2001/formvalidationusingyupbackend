466881e4b14e09cdf1fb9dae4f9422f7689cec21
someswar <someswar@klizos.com>
2025-01-31 17:53:35 +0530

    updated

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "formvalidationyupfrontend.vercel.app",
+    origin: "https://formvalidationyupfrontend.vercel.app",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
