bcdd3cb169ad3e80f745b71ae37ff25ecf426b9c
someswar <someswar@klizos.com>
2025-01-31 17:58:53 +0530

    updated

--- index.js
+++ index.js
@@ -14,6 +14,9 @@ app.use(
 connection();
 
 //Controller for registering users
+app.get("/",(req,res)=>{
+  res.status(200).json({message:"hello"})
+})
 app.use('/api',UserRegister)
 
 app.listen(3001, () => {
